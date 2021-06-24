0f2c096e8e8ca4e6f8988dca3548cd081ac17106
Koichi <koichi89592@gmail.com>
2021-06-24 18:35:08 +0900

    Add constants for driving

--- src/Car.ts
+++ src/Car.ts
@@ -8,6 +8,12 @@ export let carY = 75;
 export let carSpeed = 0;
 export let carAng = 0;
 
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.5;
+const REVERSE_POWER = 0.2;
+const TURN_RATE = 0.06;
+const MIN_SPEED_TO_TURN = 0.5;
+
 export const changeCarSpeed = () => {
   carX -= Math.cos(carAng) * carSpeed;
   carY -= Math.sin(carAng) * carSpeed;
@@ -30,22 +36,23 @@ export const carReset = () => {
 };
 
 export const carMove = () => {
-  carSpeed *= 0.97;
+  carSpeed *= GROUNDSPEED_DECAY_MULT;
 
   const SPEED_RANGE = 0.3;
   if (keyHeld_Gas) {
-    carSpeed += SPEED_RANGE;
+    carSpeed += DRIVE_POWER;
   }
   if (keyHeld_Reverse) {
-    carSpeed = carSpeed - SPEED_RANGE <= 0 ? 0 : carSpeed - SPEED_RANGE;
+    carSpeed = carSpeed - REVERSE_POWER <= 0 ? 0 : carSpeed - REVERSE_POWER;
   }
-  if (keyHeld_TurnLeft) {
-    carAng += 0.04;
+  if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
+    if (keyHeld_TurnLeft) {
+      carAng -= TURN_RATE;
+    }
+    if (keyHeld_TurnRight) {
+      carAng += TURN_RATE;
+    }
   }
-  if (keyHeld_TurnRight) {
-    carAng -= 0.04;
-  }
-
   carX += Math.cos(carAng) * carSpeed;
   carY += Math.sin(carAng) * carSpeed;
 };
